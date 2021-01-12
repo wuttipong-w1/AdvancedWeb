8dd616ed757f85cbb8a9a00ad8f75c5434cfdbc3
wuttipong <6106021622101@fitm.kmutnb.ac.th>
2021-01-12 11:28:26 +0700

    happ new year

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "wuttipong happy"
+    return "wuttipong happy new year 2021"
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port=80)
